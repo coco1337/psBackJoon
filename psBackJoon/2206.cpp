245a21c5728a36c93ab517dbc3ee3e2fada51af0
coco1337 <scar3517@gmail.com>
2021-12-14 10:30:56 +0900

    2206

--- psBackJoon/2206.cpp
+++ psBackJoon/2206.cpp
@@ -3,51 +3,57 @@
 #include <queue>
 #include <string>
 #include <algorithm>
-#define MAX 999'999'999
 
 using namespace std;
-
-vector<vector<bool>> v(1001, vector<bool>(1001));
-vector<vector<int>> chk(1001, vector<int>(1001));
 vector<int> dx = { -1, 1, 0, 0 };
 vector<int> dy = { 0, 0, 1, -1 };
-queue<pair<int, int>> changedq;
-queue<pair<int, int>> q;
+vector<vector<bool>> g(1001, vector<bool>(1001));
+vector<vector<vector<int>>> v(1001, vector<vector<int>>(1001, vector<int>(2)));
 
 // 2206 -> 14442 -> 1175
 
-int n, m, ans = MAX;
+int n, m;
 string s;
 
 bool Check(int x, int y)
 {
-  return x >= 0 && y >= 0 && x < n&& y < m && !v[x][y];
+  return x >= 0 && y >= 0 && x < n&& y < m;
 }
 
-void Bfs()
-{
-  q.push({ 0, 0 });
-  vector<vector<bool>> visited(n, vector<bool>(m));
+int Bfs() {
+  queue<pair<pair<int, int>, int>> q;
+  q.push({{ 0,0 }, 1});
+  v[0][0][1] = 1;
 
-  while (!q.empty())
-  {
-    const auto qi = q.front();
+  while (!q.empty()) {
+    int y = q.front().first.first;
+    int x = q.front().first.second;
+    int b = q.front().second;
     q.pop();
-    for (int i = 0; i < 4; ++i)
-    {
-      int a = qi.first + dx[i];
-      int b = qi.second + dy[i];
 
-      if (Check(a, b) && !visited[a][b])
-      {
-        q.push({ a, b });
-        chk[a][b] = chk[qi.first][qi.second] + 1;
-        visited[a][b] = true;
+    if (y == n - 1 && x == m - 1) {
+      return v[y][x][b];
+    }
+
+    for (int i = 0; i < 4; ++i) {
+      int ny = y + dy[i];
+      int nx = x + dx[i];
+
+      if (Check(ny, nx)) {
+        if (g[ny][nx] && b) {
+          v[ny][nx][b - 1] = v[y][x][b] + 1;
+          q.push({ {ny, nx}, b - 1 });
+        }
+
+        if (!g[ny][nx] && v[ny][nx][b] == 0) {
+          v[ny][nx][b] = v[y][x][b] + 1;
+          q.push({ {ny, nx}, b });
+        }
       }
     }
   }
-  if (visited[n - 1][m - 1])
-    ans = min(ans, chk[n - 1][m - 1]);
+
+  return -1;
 }
 
 int main() {
@@ -56,41 +62,14 @@ int main() {
 
   cin >> n >> m;
 
-  for (int i = 0; i < n; ++i)
-  {
+  for (int i = 0; i < n; ++i) {
     cin >> s;
-    for (int j = 0; j < s.size(); ++j)
-    {
-      v[i][j] = s[j] == '1';
-      if (v[i][j]) changedq.push({ i, j });
+    for (int j = 0; j < m; ++j) {
+      g[i][j] = '1' == s[j];
     }
   }
 
-  Bfs();
-
-  while (!changedq.empty())
-  {
-    const auto qi = changedq.front();
-    changedq.pop();
-    int t = 0;
-    for (int i = 0; i < 4; ++i)
-    {
-      int a = qi.first + dx[i];
-      int b = qi.second + dy[i];
-
-      if (Check(a, b)) ++t;   // 들어오는길 나가는길 있어야 의미가 있기때문에 필터
-    }
-
-    if (t > 1)
-    {
-      v[qi.first][qi.second] = false;
-      Bfs();
-      v[qi.first][qi.second] = true;
-    }
-  }
-
-  if (ans == MAX) cout << -1;
-  else cout << ans + 1;
+  cout << Bfs() << '\n';
 
   return 0;
 }
