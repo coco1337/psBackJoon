bdc618a4801fb9fff442ebd72b1f21c7578b79aa
coco1337 <scar3517@gmail.com>
2022-07-01 10:41:57 +0900

    25083

---
+++ psBackJoon/25083.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string s;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	cout << "         ,r'\"7" << endl;
+	cout << "r`-_   ,'  ,/" << endl;
+	cout << " \\. \". L_r\'" << endl;
+	cout << "   `~\\/" << endl;
+	cout << "      |" << endl;
+	cout << "      |" << endl;
+	return 0;
+}
