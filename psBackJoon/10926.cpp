f5d6b3c360a60732aacedb2d2ffab0da3648d41d
coco1337 <scar3517@gmail.com>
2022-07-01 10:28:04 +0900

    10926

---
+++ psBackJoon/10926.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string s;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cin >> s;
+
+	cout << s << "??!" << endl;
+
+	return 0;
+}
