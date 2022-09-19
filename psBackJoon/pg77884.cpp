56ffc36374c294d09917f108fbb00c585d10928f
coco1337 <scar3517@gmail.com>
2022-09-19 15:48:27 +0900

    programmers 77884

---
+++ psBackJoon/pg77884.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int solution(int left, int right) {
+    int answer = 0;
+    
+    for (int i = left; i <= right; ++i) {
+        int tmp = sqrt(i);
+        if (tmp * tmp == i) answer -= i;
+        else answer += i;
+    }
+    return answer;
+}
